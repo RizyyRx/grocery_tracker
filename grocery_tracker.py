fbb9e0cabc52376262cfabf6da0ddceeceadbc02
Rizwanudeen K <rizwankendo@gmail.com>
2024-06-30 18:13:48 +0000

    Added simple expiry detection function

--- grocery_tracker.py
+++ grocery_tracker.py
@@ -6,14 +6,11 @@ Functionalities of grocery tracker:
 """
 import time 
 
-# current_time=time.time()
-# print(current_time)
+# current_time_epoch=int(time.time())
+# struct_time=(time.localtime(current_time_epoch))
+# current_day=time.strftime("%d",struct_time)
+# current_time_seconds=time.strftime("%s",struct_time)
 
-# struct_time=(time.localtime(current_time))
-# print(struct_time)
-
-# formatted_time=time.strftime("%d",struct_time)
-# print(formatted_time)
 
 #initialize dictionaries to store expiry and count info of groceries
 groceries_expiry={}
@@ -23,15 +20,37 @@ groceries_count={}
 def add_grocery():
 
     #prompt user for name,count and expiry time
+    #using less seconds as expiry time for easy testing
     name=str(input("Enter a grocery name :"))
     count=int(input("Enter "+name+" count :"))
-    expiry_time=int(input("In how many days will the "+name+" get expired? :"))
+    expiry_time=int(input("In how many seconds will the "+name+" get expired? :"))
 
-    #set the data to dict
-    groceries_expiry[name]=[expiry_time]
+    #set current date, expiry date and count to dictionary
+    groceries_expiry[name]=[int(time.time()),expiry_time]
     groceries_count[name]=[count]
 
+#checks grocery expiry status
+def check_grocery():
+
+    #prompt user to check grocery 
+    name=str(input("Enter grocery name to check:"))
+
+
+    if name in groceries_expiry:
+        grocery_expiry_info=groceries_expiry[name]
+        exact_expiring_time=grocery_expiry_info[0]+grocery_expiry_info[1]
+        while int(time.time())<exact_expiring_time:
+            print("initial time: "+str(grocery_expiry_info[0]))
+            print("exact expiring time: "+str(exact_expiring_time))
+            print("current time: "+str(int(time.time())))
+            print(name+" is not expired\n")
+            time.sleep(1)
+        print(name+" is expired!!!! GG's amigo, nt though")
+    else:
+        print("The grocery "+name+" is not available")
+
 add_grocery()
-print("Grocery count= "+str(groceries_count))
-print("Grocery expiry info= "+str(groceries_expiry))
+check_grocery()
+# print("Grocery count= "+str(groceries_count))
+# print("Grocery expiry info= "+str(groceries_expiry))
 
